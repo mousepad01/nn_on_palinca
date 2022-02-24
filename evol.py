95757d6566d7c664535a06d1848a63902856d664
mousepad01 <acalin2000@gmail.com>
2022-02-24 11:52:07 +0200

    minor

--- evol.py
+++ evol.py
@@ -155,7 +155,7 @@ class Evolvable:
         """chromosome with all bits set to 1"""
 
         self.state = EvolState.UNINITIALIZED
-        self.learner: HumanVsRnd = None
+        self.learner: KeystrokeFingerprintClassificator = None
         """associated model from learner.py"""
 
         self.elite_cnt = elite_cnt
